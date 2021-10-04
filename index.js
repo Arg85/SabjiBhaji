59956ecb3c9053433246f9302b9952976ce81f2c
Anurag Yadav <anuragy554@gmail.com>
2021-10-04 18:05:31 +0530

    axios removed

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ const axios=require("axios")
 app.get('/appy',(req,res)=>{
     res.send("hello world2")
 })
-app.get('/users',async(req,res)=>{
-    let ar=[]
-axios.get("https://jsonplaceholder.typicode.com/posts/1").then((resp)=>{
-res.send(resp.data.body)
-}) .catch((a)=>{
-    console.log(a)
-}) 
-
-   
-
-
-})
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
