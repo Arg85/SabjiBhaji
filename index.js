5979c05f4387d9f255c57b687b705632729bc198
Anurag Yadav <anuragy554@gmail.com>
2021-10-04 18:18:23 +0530

    eslint added to enfoce standard code style

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const express=require('express')
-const app=express();
-const axios=require("axios")
-
-app.get('/appy',(req,res)=>{
-    res.send("hello world2")
+const express = require('express')
+const app = express()
+app.get('/appy', (req, res) => {
+  res.send('hello world2')
 })
-app.get('/',(req,res)=>{
-    res.send("hello world")
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
-app.listen(4000,()=>{
-    const PORT=process.env.PORT || 4000
-    console.log(`server running at ${PORT}`)
-});
-
-
-
+app.listen(4000, () => {
+  const PORT = process.env.PORT || 4000
+  console.log(`server running at ${PORT}`)
+})
