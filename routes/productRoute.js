879a0598245b1a6fa4988c85185889a7ae14f8a2
Anurag Yadav <anuragy554@gmail.com>
2021-10-17 15:54:43 +0530

    17-10-2021

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -17,7 +17,7 @@ const router = require('express').Router()
 router.post('/productCategory', isAuth, upload.single('image'), productController.addProductCategory)
 router.delete('/productCategory/:categoryId', isAuth, productController.deleteProductCategory)
 router.put('/productCategory/:categoryId', isAuth, upload.single('image'), productController.updateProductCategory)
-// router.post('/seed/product', isAuth, productController.addProduct)
+router.post('/seed/product', isAuth, upload.single('image'), productController.addProduct)
 router.get('/productCategory', productController.productCategories)
 
 module.exports = router
